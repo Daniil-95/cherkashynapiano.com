1fc58001306d24e71a37426eeab91448ee16b8ed
Daniil Andrushko <dan007sar@gmail.com>
2026-06-02 18:19:22 +0200

    Vytvořena základní struktura projektu (Next.js, Strapi, Prisma) p.2

--- packages/cms/config/admin.js
+++ packages/cms/config/admin.js
@@ -1,21 +1,21 @@
-import path from "path";
-
-export default {
+module.exports = ({ env }) => ({
   admin: {
     auth: {
-      secret: process.env.ADMIN_JWT_SECRET || "supersecretjwttoken",
+      secret: env('ADMIN_JWT_SECRET') || 'MjAyNS1jaGVya2FzaHluYXBpYW5vLWFkbWluLXNlY3JldC1rZXktMzItY2hhcnM=',
     },
   },
   apiToken: {
-    salt: process.env.API_TOKEN_SALT || "supersecretapitokensalt",
+    salt: env('API_TOKEN_SALT') || 'c2FsdC1mb3ItYXBpLXRva2VuLWtleTMyLWNoYXJzLWhlcmUtMjAyNQ==',
   },
   transfer: {
     token: {
-      salt: process.env.TRANSFER_TOKEN_SALT || "supersecrettransfertokensalt",
+      salt: env('TRANSFER_TOKEN_SALT') || 'dHJhbnNmZXItc2FsdC1rZXktMzItY2hhcnMtaGVyZS0yMDI1LXN0cmFwaQ==',
     },
   },
   flags: {
-    nps: process.env.FLAG_NPS !== "false",
-    promoteEE: process.env.FLAG_PROMOTE_EE !== "false",
+    nps: env.bool('FLAG_NPS', false),
+    promoteEE: env.bool('FLAG_PROMOTE_EE', false),
   },
-};
+});
+
+
--- packages/cms/config/plugins.js
+++ packages/cms/config/plugins.js
@@ -1,10 +1,8 @@
 module.exports = ({ env }) => ({
   email: {
     config: {
-      provider: "sendgrid",
-      providerOptions: {
-        apiKey: env("SENDGRID_API_KEY"),
-      },
+      provider: "sendmail",
+      providerOptions: {},
       settings: {
         defaultFrom: env("EMAIL_FROM", "noreply@cherkashynapiano.com"),
         defaultReplyTo: env("EMAIL_REPLY_TO", "info@cherkashynapiano.com"),
