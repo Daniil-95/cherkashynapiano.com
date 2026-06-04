b27f3061e7eca7e4dc568bc88754d2082ee0d0c8
Daniil Andrushko <dan007sar@gmail.com>
2026-06-04 13:37:32 +0200

    Refaktoring a čištení kodu

--- packages/cms/config/admin.js
+++ packages/cms/config/admin.js
@@ -1,21 +1,26 @@
 module.exports = ({ env }) => ({
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET') || 'MjAyNS1jaGVya2FzaHluYXBpYW5vLWFkbWluLXNlY3JldC1rZXktMzItY2hhcnM=',
-    },
+  auth: {
+    secret:
+      env("ADMIN_JWT_SECRET") ||
+      "MjAyNS1jaGVya2FzaHluYXBpYW5vLWFkbWluLXNlY3JldC1rZXktMzItY2hhcnM=",
   },
+
   apiToken: {
-    salt: env('API_TOKEN_SALT') || 'c2FsdC1mb3ItYXBpLXRva2VuLWtleTMyLWNoYXJzLWhlcmUtMjAyNQ==',
+    salt:
+      env("API_TOKEN_SALT") ||
+      "c2FsdC1mb3ItYXBpLXRva2VuLWtleTMyLWNoYXJzLWhlcmUtMjAyNQ==",
   },
+
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT') || 'dHJhbnNmZXItc2FsdC1rZXktMzItY2hhcnMtaGVyZS0yMDI1LXN0cmFwaQ==',
+      salt:
+        env("TRANSFER_TOKEN_SALT") ||
+        "dHJhbnNmZXItc2FsdC1rZXktMzItY2hhcnMtaGVyZS0yMDI1LXN0cmFwaQ==",
     },
   },
+
   flags: {
-    nps: env.bool('FLAG_NPS', false),
-    promoteEE: env.bool('FLAG_PROMOTE_EE', false),
+    nps: env.bool("FLAG_NPS", false),
+    promoteEE: env.bool("FLAG_PROMOTE_EE", false),
   },
 });
-
-
